183632775b028ac9681ec060d6f4fe2f902c0c05
mohammad sorooshfar <mohammad.sorooshfar@yahoo.com>
2022-05-03 11:37:45 +0430

    add algorithm

--- ex13.js
+++ ex13.js
@@ -4,14 +4,12 @@ function tellDate(number) {
     number -= 186;
     season = 2 + Math.ceil(number / 90);
     month = 6 + Math.ceil(number / 30);
-    let n = Math.floor(number / 30);
-    day = number - n * 30;
+    day = number % 30 == 0 ? 30 : number % 30;
   } else {
     season = Math.ceil(number / 93);
     month = Math.ceil(number / 31);
-    let n = Math.floor(number / 31);
-    day = number == n * 31 ? 31 : number - n * 31;
+    day = number % 31 == 0 ? 31 : number % 31;
   }
   return { season, month, day };
 }
-console.log(tellDate(48));
+console.log(tellDate(217));
